fb4f2f9f9644fff62acdafa19bf25d62568f7ebe
joao-freitas-nexaas <master.007,1>
2020-10-05 09:23:29 -0300

    refactor

--- app/src/main/java/br/com/dio/picpayclone/services/RetrofitService.kt
+++ app/src/main/java/br/com/dio/picpayclone/services/RetrofitService.kt
@@ -3,7 +3,7 @@ package br.com.dio.picpayclone.services
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val URL = "http://192.168.0.27:8080"
+private const val URL = "http://10.0.0.112:8080"
 
 object RetrofitService {
 
